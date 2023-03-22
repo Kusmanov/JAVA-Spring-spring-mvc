15cc04a7e1adc4aee1542750256ffdcaf12e4902
Kusmanov <b.kusmanov@gmail.com>
2023-03-22 11:23:27 +0600

    index.jsp corrected

--- src/main/java/org/skillfactory/HelloController.java
+++ src/main/java/org/skillfactory/HelloController.java
@@ -3,19 +3,27 @@ package org.skillfactory;
 import org.skillfactory.dto.StudentData;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 @RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping(value = "/student", method = RequestMethod.GET) //Можно поменять на @GetMapping("/student")
-    //@ResponseBody
-    public String helloStudent() {
-        //return "Hello, student!";
-        return "index";
+//    @RequestMapping(value = "/student", method = RequestMethod.GET) //Можно поменять на @GetMapping("/student")
+//    @ResponseBody
+//    public String helloStudent() {
+//        //return "Hello, student!";
+//        //return "index";
+//    }
+
+    @GetMapping("/student")
+    public ModelAndView myData() {
+        ModelAndView mav = new ModelAndView("index");
+        mav.addObject("myData", "Hello, student!");
+        return mav;
     }
 
-    @RequestMapping(value  = "/greetings/{name}")
+    @RequestMapping(value = "/greetings/{name}")
     public String greetingsByName(@PathVariable String name) {
         return "Hello, " + name;
     }
